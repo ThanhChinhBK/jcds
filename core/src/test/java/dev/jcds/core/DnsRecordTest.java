adb3df6b7b31f96b37bb154391664aa6a258e05e
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2022-09-19 13:51:48 +0900

    implement testcase for DNSRECORD

---
+++ core/src/test/java/dev/jcds/core/DnsRecordTest.java
@@ -0,0 +1,46 @@
+package dev.jcds.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static dev.jcds.core.QueryType.*;
+
+public class DnsRecordTest {
+    @Test
+    public void testDnsRecord() throws IOException {
+        byte[] buf = Files.readAllBytes(Path.of("src/test/resources/response_packet.txt"));
+        BytePacketBuffer buffer = new BytePacketBuffer();
+        buffer.setBuf(buf);
+        DnsPacket packet = DnsPacket.fromBuffer(buffer);
+
+        // test header
+        assert packet.header.id == 60744;
+        assert packet.header.recursionDesired;
+        assert !packet.header.truncatedMessage;
+        assert !packet.header.authoritativeAnswer;
+        assert packet.header.opcode == 0;
+        assert packet.header.response;
+        assert packet.header.resultCode == ResultCode.NO_ERROR;
+        assert !packet.header.checkingDisabled;
+        assert !packet.header.authenticatedData;
+        assert !packet.header.z;
+        assert packet.header.recursionAvailable;
+
+        // test questions
+        assert packet.header.questionCount == 1;
+        DnsQuestion question = packet.questions.get(0);
+        assert question.qname.equals("google.com");
+
+        // test answers
+        assert packet.header.answerCount == 1;
+        DnsRecord record = packet.answers.get(0);
+        assert record.domain.equals("google.com");
+
+
+
+
+    }
+}
