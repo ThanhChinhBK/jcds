0fa24a1e07730d28813ed425cf4ef8431c6bd65b
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2022-10-21 01:00:53 +0900

    Implement stub resolver

--- core/src/test/java/dev/jcds/core/StubResolverTest.java
+++ core/src/test/java/dev/jcds/core/StubResolverTest.java
@@ -2,6 +2,7 @@ package dev.jcds.core;
 
 import dev.jcds.core.query.AQueryType;
 import dev.jcds.core.query.QueryType;
+import dev.jcds.core.record.DnsRecord;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -30,28 +31,66 @@ public class StubResolverTest {
         packet.header.recursionDesired = true;
         packet.questions.add(new DnsQuestion(qname, qtype));
 
+        // Use our new write method to write the packet to a buffer...
+        BytePacketBuffer buffer = new BytePacketBuffer();
+        packet.write(buffer);
+
         // Use the socket to send the query
         BytePacketBuffer requestBuffer = new BytePacketBuffer();
+        DatagramPacket requestPacket = new DatagramPacket(
+                buffer.getBuf(),
+                buffer.getBuf().length,
+                InetAddress.getByName(serverIp),
+                serverPort
+        );
         socket.send(
-                new DatagramPacket(
-                        requestBuffer.getBuf(),
-                        requestBuffer.getBuf().length,
-                        InetAddress.getByName(serverIp),
-                        serverPort
-                )
+                requestPacket
         );
 
-        // Receive the response
+        // To prepare for receiving the response, we'll create a new `BytePacketBuffer`,
+        // and ask the socket to write the response directly into our buffer.
         BytePacketBuffer responseBuffer = new BytePacketBuffer();
+        DatagramPacket responsePacket = new DatagramPacket(
+                responseBuffer.getBuf(),
+                responseBuffer.getBuf().length
+        );
         socket.receive(
-                new DatagramPacket(
-                        responseBuffer.getBuf(),
-                        responseBuffer.getBuf().length
-                )
+                responsePacket
         );
 
-        // Parse the response
+        // get response buffer
+        responseBuffer.setBuf(responsePacket.getData());
+
+
+        // As per the previous section, `DnsPacket::from_buffer()` is then used to
+        // actually parse the packet after which we can print the response.
         DnsPacket response = DnsPacket.fromBuffer(responseBuffer);
+
+        // Test header
         assert response.header.id == 6666;
+        assert response.header.recursionDesired;
+        assert !response.header.truncatedMessage;
+        assert !response.header.authoritativeAnswer;
+        assert response.header.opcode == 0;
+        assert response.header.response;
+        assert response.header.resultCode == ResultCode.NO_ERROR;
+        assert !response.header.checkingDisabled;
+        assert !response.header.authenticatedData;
+        assert !response.header.z;
+        assert response.header.recursionAvailable;
+        assert response.header.questionCount == 1;
+        assert response.header.answerCount == 1;
+        assert response.header.authorityEntriesCount == 0;
+        assert response.header.resourceEntriesCount == 0;
+
+
+        // Test questions
+        DnsQuestion question = response.questions.get(0);
+        assert question.qname.equals(qname);
+        assert question.qtype.toString().equals("A");
+
+        // Test answers
+        DnsRecord record = response.answers.get(0);
+        assert record.getDomain().equals(qname);
     }
 }
