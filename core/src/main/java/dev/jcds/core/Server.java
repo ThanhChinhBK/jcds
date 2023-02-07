d75500c82642b413fac9834c1d8d0aad40f9bfc2
ThanhChinh.Nguyen <nguyenthanhchinh96@gmail.com>
2023-02-08 02:02:53 +0900

    implement lookup function

--- core/src/main/java/dev/jcds/core/Server.java
+++ core/src/main/java/dev/jcds/core/Server.java
@@ -1,41 +1,45 @@
 package dev.jcds.core;
 
 import dev.jcds.core.query.QueryType;
+import dev.jcds.core.record.DnsRecord;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
 
 public class Server {
     private DnsPacket lookup(String qname, QueryType qtype) throws IOException {
-        String dnsServer = "8.8.8.8";
-        int dnsPort = 53;
+        InetSocketAddress server = new InetSocketAddress("8.8.8.8", 53);
         DatagramSocket socket = new DatagramSocket(null);
-        socket.bind(
-                new InetSocketAddress(dnsServer, dnsPort)
-        );
+        DnsPacket packet = new DnsPacket();
 
-        DnsPacket dnsPacket = new DnsPacket();
-        dnsPacket.header.id = 6666;
-        dnsPacket.header.questionCount = 1;
-        dnsPacket.header.recursionDesired = true;
-        dnsPacket.questions.add(new DnsQuestion(qname, qtype));
+        packet.header.id = 6666;
+        packet.header.questionCount = 1;
+        packet.header.recursionDesired = true;
+        packet.questions.add(new DnsQuestion(qname, qtype));
+
         BytePacketBuffer requestBuffer = new BytePacketBuffer();
-        dnsPacket.write(requestBuffer);
-        socket.send(requestBuffer.getPacket(dnsServer, dnsPort));
+        packet.write(requestBuffer);
+        socket.send(new DatagramPacket(
+                requestBuffer.getBuf(),
+                requestBuffer.getBuf().length,
+                server
+        ));
 
         BytePacketBuffer responseBuffer = new BytePacketBuffer();
-        dnsPacket.write(responseBuffer);
-
         DatagramPacket responsePacket = new DatagramPacket(
                 responseBuffer.getBuf(),
                 responseBuffer.getBuf().length
         );
         socket.receive(responsePacket);
+        socket.close();
         responseBuffer.setBuf(responsePacket.getData());
-        dnsPacket.write(responseBuffer);
-        return dnsPacket;
+        return DnsPacket.fromBuffer(responseBuffer);
     }
+
+
 }
